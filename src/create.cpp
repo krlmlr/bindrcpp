230a22e99b84ea98c1ceb04c5ab89f96fcf13849
Kirill Müller <krlmlr+r@mailbox.org>
2016-10-14 23:34:28 +0200

    use sugar

--- src/create.cpp
+++ src/create.cpp
@@ -7,10 +7,7 @@ using namespace Rcpp;
 // [[Rcpp::interfaces(cpp)]]
 // [[Rcpp::export]]
 SEXP create_environment(CharacterVector names, XPtr<GETTER_FUNC> fun, XPtr<void*> payload, Environment parent) {
-  List fun_payload(2);
-  fun_payload[0] = fun;
-  fun_payload[1] = payload;
-  fun_payload.names() = CharacterVector("fun", "payload");
+  List fun_payload = List::create(_["fun"] = fun, _["payload"] = payload);
 
   Environment package("package:RcppActiveBinding");
   Function create_callback_environment = package["create_callback_environment"];
