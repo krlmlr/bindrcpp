74e33f5df82d7ef680619e6a33d38eb6861e44dc
Kirill Müller <krlmlr+r@mailbox.org>
2016-10-15 00:51:29 +0200

    use heap pointer, store XPtr in parent env to avoid memory leak

--- src/create.cpp
+++ src/create.cpp
@@ -2,12 +2,15 @@
 
 #include <RcppActiveBinding.h>
 
+#include <plogr.h>
+
 using namespace Rcpp;
 
 // [[Rcpp::interfaces(cpp)]]
 // [[Rcpp::export]]
 SEXP create_environment(CharacterVector names, XPtr<GETTER_FUNC> fun, XPtr<PAYLOAD> payload, Environment parent) {
   List fun_payload = List::create(_["fun"] = fun, _["payload"] = payload);
+  LOG_VERBOSE << payload.get();
 
   Environment package("package:RcppActiveBinding");
   Function create_callback_environment = package["create_callback_environment"];
--- src/test.cpp
+++ src/test.cpp
@@ -2,6 +2,8 @@
 
 #include <RcppActiveBinding.h>
 
+#include <plogr.h>
+
 #include <algorithm>
 #include <string>
 
@@ -9,63 +11,82 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 SEXP callback(String name, List fun_payload) {
-  XPtr<GETTER_FUNC> funx = fun_payload["fun"];
-  XPtr<void*> payload = fun_payload["payload"];
+  XPtr<GETTER_FUNC> xfun = fun_payload["fun"];
+  XPtr<PAYLOAD> xpayload = fun_payload["payload"];
 
-  GETTER_FUNC* fun = funx.get();
-  return (*fun)(name, payload);
+  GETTER_FUNC* pfun = xfun.get();
+  PAYLOAD* payload = xpayload.get();
+
+  LOG_VERBOSE << payload;
+
+  return (*pfun)(name, *payload);
 }
+
+class CallbackTester {
+  enum { MAGIC = 20161014 };
+  const int magic;
+
+public:
+  CallbackTester() : magic(MAGIC) { LOG_VERBOSE; }
+  ~CallbackTester() { LOG_VERBOSE; }
+
+  static SEXP tolower_static(Rcpp::String name, PAYLOAD payload) {
+    LOG_VERBOSE << payload;
+    CallbackTester* this_ = reinterpret_cast<CallbackTester*>(payload);
+    return this_->tolower(name);
+  }
+
+  static SEXP toupper_static(Rcpp::String name, PAYLOAD payload) {
+    LOG_VERBOSE << payload;
+    CallbackTester* this_ = reinterpret_cast<CallbackTester*>(payload);
+    return this_->toupper(name);
+  }
+
+private:
+  SEXP tolower(Rcpp::String name) {
+    LOG_VERBOSE << magic;
+    if (magic != MAGIC)
+      stop("payload lost");
+    std::string name_string = name;
+    std::transform(name_string.begin(), name_string.end(), name_string.begin(), ::tolower);
+    return CharacterVector(name);
+  }
+
+  SEXP toupper(Rcpp::String name) {
+    LOG_VERBOSE << magic;
+    if (magic != MAGIC)
+      stop("payload lost");
+    std::string name_string = name;
+    std::transform(name_string.begin(), name_string.end(), name_string.begin(), ::toupper);
+    return CharacterVector(name);
+  }
+};
 
 // [[Rcpp::export]]
 SEXP do_test_create_environment(CharacterVector names, String xform, Environment parent) {
-  class callback {
-    enum { MAGIC = 20161014 };
+  CallbackTester* pc = new CallbackTester;
 
-  public:
-    callback() : magic(MAGIC) {}
+  // HACK: Insert into parent environment to avoid early destruction or memory leaks.
+  // A real application would probably store this object elsewhere
+  parent["__CALLBACK_STORAGE__"] = XPtr<CallbackTester>(pc);
 
-    static SEXP tolower_static(Rcpp::String name, void* payload) {
-      callback* this_ = reinterpret_cast<callback*>(payload);
-      return this_->tolower(name);
-    }
-
-    static SEXP toupper_static(Rcpp::String name, void* payload) {
-      callback* this_ = reinterpret_cast<callback*>(payload);
-      return this_->toupper(name);
-    }
-
-  private:
-    SEXP tolower(Rcpp::String name) {
-      if (magic != MAGIC)
-        stop("payload lost");
-      std::string name_string = name;
-      std::transform(name_string.begin(), name_string.end(), name_string.begin(), ::tolower);
-      return CharacterVector(name);
-    }
-
-    SEXP toupper(Rcpp::String name) {
-      if (magic != MAGIC)
-        stop("payload lost");
-      std::string name_string = name;
-      std::transform(name_string.begin(), name_string.end(), name_string.begin(), ::toupper);
-      return CharacterVector(name);
-    }
-
-  private:
-    int magic;
-  };
-
-  callback c;
+  CallbackTester& c = *pc;
 
   if (xform == "tolower") {
+    PAYLOAD* ppayload = new PAYLOAD(&c);
+    LOG_VERBOSE << ppayload;
+
     return RcppActiveBinding::create_environment(
-      names, XPtr<GETTER_FUNC>(new GETTER_FUNC(&callback::tolower_static)),
-      XPtr<void*>(new void*(&c)), parent);
+      names, XPtr<GETTER_FUNC>(new GETTER_FUNC(&CallbackTester::tolower_static)),
+      XPtr<PAYLOAD>(ppayload), parent);
   }
   else if (xform == "toupper") {
+    PAYLOAD* ppayload = new PAYLOAD(&c);
+    LOG_VERBOSE << ppayload;
+
     return RcppActiveBinding::create_environment(
-      names, XPtr<GETTER_FUNC>(new GETTER_FUNC(&callback::toupper_static)),
-      XPtr<void*>(new void*(&c)), parent);
+      names, XPtr<GETTER_FUNC>(new GETTER_FUNC(&CallbackTester::toupper_static)),
+      XPtr<PAYLOAD>(ppayload), parent);
   }
   else
     stop("unknown xform");
