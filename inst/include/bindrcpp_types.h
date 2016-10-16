3e39244a0a06e18cc7010db4226b8f377cf1b4d8
Kirill Müller <krlmlr+r@mailbox.org>
2016-10-16 03:10:48 +0200

    include conversion routines locally

--- inst/include/bindrcpp_types.h
+++ inst/include/bindrcpp_types.h
@@ -14,12 +14,29 @@ typedef SEXP (*GETTER_FUNC_SYMBOL)(const Rcpp::Symbol& name, bindrcpp::PAYLOAD p
 }
 
 namespace Rcpp {
-  template <> SEXP wrap(const bindrcpp::PAYLOAD&);
-  template <> SEXP wrap(const bindrcpp::GETTER_FUNC_STRING&);
-  template <> SEXP wrap(const bindrcpp::GETTER_FUNC_SYMBOL&);
-  template <> bindrcpp::PAYLOAD as(SEXP);
-  template <> bindrcpp::GETTER_FUNC_STRING as(SEXP);
-  template <> bindrcpp::GETTER_FUNC_SYMBOL as(SEXP);
+  using namespace bindrcpp;
+
+  template <> inline SEXP wrap(const PAYLOAD& payload) {
+    return XPtr<PAYLOAD>(new PAYLOAD(payload));
+  }
+  template <> inline SEXP wrap(const GETTER_FUNC_STRING& fun) {
+    return XPtr<GETTER_FUNC_STRING>(new GETTER_FUNC_STRING(fun));
+  }
+  template <> inline SEXP wrap(const GETTER_FUNC_SYMBOL& fun) {
+    return XPtr<GETTER_FUNC_SYMBOL>(new GETTER_FUNC_SYMBOL(fun));
+  }
+  template <> inline PAYLOAD as(SEXP x) {
+    XPtr<PAYLOAD> xpayload(x);
+    return *xpayload.get();
+  }
+  template <> inline GETTER_FUNC_STRING as(SEXP x) {
+    XPtr<GETTER_FUNC_STRING> xfun(x);
+    return *xfun.get();
+  }
+  template <> inline GETTER_FUNC_SYMBOL as(SEXP x) {
+    XPtr<GETTER_FUNC_SYMBOL> xfun(x);
+    return *xfun.get();
+  }
 }
 
 #endif
