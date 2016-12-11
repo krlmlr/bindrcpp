773dcb9e6cc6d8045c05b3e9401bef344fb6efaa
Kirill Müller <krlmlr+r@mailbox.org>
2016-12-11 13:06:53 +0000

    Deploy from Travis build 39 [ci skip]
    Build URL: https://travis-ci.org/krlmlr/bindrcpp/builds/183009506
    Commit: bc098fa515e76c41f50287b7bed29fa86e226d4e

--- README_cache/html/module_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-0.12.8.2/sourcecpp_41be447aa6e9/file41be1501a5be.cpp.R
+++
@@ -1,5 +0,0 @@
-`.sourceCpp_1_DLLInfo` <- dyn.load('/home/travis/build/krlmlr/bindrcpp/docs/README_cache/html/module_sourceCpp/sourceCpp-x86_64-pc-linux-gnu-0.12.8.2/sourcecpp_41be447aa6e9/sourceCpp_2.so')
-
-test_tolower_bindings <- Rcpp:::sourceCppFunction(function(names, parent) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_test_tolower_bindings')
-
-rm(`.sourceCpp_1_DLLInfo`)
