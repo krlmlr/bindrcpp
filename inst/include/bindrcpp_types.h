bbecf78f3d4753a74199b17f8ecbd6fab553c6bd
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-08 16:36:21 +0200

    wrap function pointer in struct
    the old code was not portable

--- inst/include/bindrcpp_types.h
+++ inst/include/bindrcpp_types.h
@@ -24,10 +24,48 @@ struct PAYLOAD {
 typedef SEXP (*GETTER_FUNC_STRING)(const Rcpp::String& name, bindrcpp::PAYLOAD payload);
 typedef SEXP (*GETTER_FUNC_SYMBOL)(const Rcpp::Symbol& name, bindrcpp::PAYLOAD payload);
 
-}
+namespace internal {
+
+struct GETTER_FUNC_STRING_WRAP {
+  GETTER_FUNC_STRING p;
+
+  explicit GETTER_FUNC_STRING_WRAP(GETTER_FUNC_STRING p_) : p(p_) {
+    LOG_VERBOSE;
+  }
+
+  GETTER_FUNC_STRING_WRAP(const GETTER_FUNC_STRING_WRAP& p_) : p(p_.p) {
+    LOG_VERBOSE;
+  }
+
+  ~GETTER_FUNC_STRING_WRAP() {
+    LOG_VERBOSE;
+  }
+};
+
+struct GETTER_FUNC_SYMBOL_WRAP {
+  GETTER_FUNC_SYMBOL p;
+
+  explicit GETTER_FUNC_SYMBOL_WRAP(GETTER_FUNC_SYMBOL p_) : p(p_) {
+    LOG_VERBOSE;
+  }
+
+  GETTER_FUNC_SYMBOL_WRAP(const GETTER_FUNC_SYMBOL_WRAP& p_) : p(p_.p) {
+    LOG_VERBOSE;
+  }
+
+  ~GETTER_FUNC_SYMBOL_WRAP() {
+    LOG_VERBOSE;
+  }
+};
+
+} // namespace internal
+
+} // namespace bindrcpp
 
 namespace Rcpp {
   using namespace bindrcpp;
+
+  using namespace bindrcpp::internal;
 
   template <> inline SEXP wrap(const PAYLOAD& payload) {
     LOG_VERBOSE << "wrap(PAYLOAD)\n";
@@ -35,11 +73,15 @@ namespace Rcpp {
   }
   template <> inline SEXP wrap(const GETTER_FUNC_STRING& fun) {
     LOG_VERBOSE << "wrap(GETTER_FUNC_STRING)\n";
-    return List::create(XPtr<GETTER_FUNC_STRING>(new GETTER_FUNC_STRING(fun)));
+    SEXP ret = List::create(XPtr<GETTER_FUNC_STRING_WRAP>(new GETTER_FUNC_STRING_WRAP(fun)));
+    LOG_VERBOSE << "wrap(GETTER_FUNC_STRING) done\n";
+    return ret;
   }
   template <> inline SEXP wrap(const GETTER_FUNC_SYMBOL& fun) {
     LOG_VERBOSE << "wrap(GETTER_FUNC_SYMBOL)\n";
-    return List::create(XPtr<GETTER_FUNC_SYMBOL>(new GETTER_FUNC_SYMBOL(fun)));
+    SEXP ret = List::create(XPtr<GETTER_FUNC_SYMBOL_WRAP>(new GETTER_FUNC_SYMBOL_WRAP(fun)));
+    LOG_VERBOSE << "wrap(GETTER_FUNC_SYMBOL) done\n";
+    return ret;
   }
   template <> inline PAYLOAD as(SEXP x) {
     SEXP x0 = VECTOR_ELT(x, 0);
@@ -50,12 +92,14 @@ namespace Rcpp {
   template <> inline GETTER_FUNC_STRING as(SEXP x) {
     LOG_VERBOSE << "GETTER_FUNC_STRING as()\n";
     SEXP x0 = VECTOR_ELT(x, 0);
-    return *(GETTER_FUNC_STRING*)R_ExternalPtrAddr(x0);
+    GETTER_FUNC_STRING_WRAP* p = (GETTER_FUNC_STRING_WRAP*)R_ExternalPtrAddr(x0);
+    return p->p;
   }
   template <> inline GETTER_FUNC_SYMBOL as(SEXP x) {
     LOG_VERBOSE << "GETTER_FUNC_SYMBOL as()\n";
     SEXP x0 = VECTOR_ELT(x, 0);
-    return *(GETTER_FUNC_SYMBOL*)R_ExternalPtrAddr(x0);
+    GETTER_FUNC_SYMBOL_WRAP* p = (GETTER_FUNC_SYMBOL_WRAP*)R_ExternalPtrAddr(x0);
+    return p->p;
   }
 }
 
